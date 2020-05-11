698126f8e4217836ab8b3140c75ab05ddfd68a0f
Fredy-SSA <alin@zizo.ro>
2020-05-11 15:18:30 +0300

    json

--- M1.ps1
+++ M1.ps1
@@ -12,6 +12,8 @@ Get-Module -ListAvailable
 
 Get-Command -Module MicrosoftTeams
 
+Connect-MicrosoftTeams
+
 help Connect-MicrosoftTeams -ShowWindow 
 
 help New-Team  -ShowWindow 
@@ -28,3 +30,28 @@ SUport Visual C++: https://support.microsoft.com/en-us/help/2977003/the-latest-s
 
 #>
 
+Import-Module SkypeOnlineConnector
+
+$Session = New-CsOnlineSession
+
+Import-PSSession $Session
+
+Get-Command -Module <Name> # Module name
+#For example: Get-Command -Module tmp_f1kjmla4.h1v
+
+Get-Help Get-CsTeamsMessagingPolicy
+
+Get-command 
+Get-help 
+Get-help about_*
+Get-help about_ForEach -ShowWindow 
+
+
+
+Get-Command *json*
+
+Get-Team | ConvertTo-Json >> C:\Alin\Teams\jsonsample.txt
+ise C:\Alin\Teams\jsonsample.txt 
+
+$date = Get-Content C:\Alin\Teams\jsonsample.txt 
+$date | ConvertFrom-Json | ft 
